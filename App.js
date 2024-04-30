680e0c455dd6a555eb821642bca5144344cf989e
charans29 <smodugula@csuchico.edu>
2024-04-29 21:08:16 -0700

    tailwindcss setup

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex items-center top-11">
+      <Text className="text-pink-600 top-11">Open up App.js toddddd start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
